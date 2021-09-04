93317282260f7160c18e07d4a053c7527d5e5540
Gadiyatov Anton <gadiyatov.an@gmail.com>
2021-09-04 17:33:01 +0500

    feat: add gessing-game logic

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.gMax = max;
+        this.gMin = min;
     }
 
     guess() {
-
+        this.gNumber = Math.ceil((this.gMin + this.gMax)/2);    
+        return this.gNumber;
     }
 
     lower() {
-
+        this.gMax = this.gNumber;
     }
 
     greater() {
-
+        this.gMin = this.gNumber;
     }
 }
 
